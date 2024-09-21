d9d2cd99380d721c323b0d2926e9a0e58b08c660
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-21 05:13:31 +0200

    Construction du tableau de bord

--- src/Twig/Components/NewUtilisateur.php
+++ src/Twig/Components/NewUtilisateur.php
@@ -2,20 +2,29 @@
 
 namespace App\Twig\Components;
 
+use DateTimeImmutable;
 use App\Entity\Utilisateur;
 use App\Entity\UtilisateurJSB;
-use DateTimeImmutable;
+use App\Repository\UtilisateurJSBRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 
-#[AsLiveComponent()]
-class NewUtilisateur
+#[AsLiveComponent(csrf: false)]
+// #[AsLiveComponent()]
+class NewUtilisateur extends AbstractController
 {
     use DefaultActionTrait;
 
+    #[LiveProp]
     public bool $saved = false;
+
+    public bool $validated = false;
 
     #[LiveProp(writable: [
         'nom',
@@ -25,7 +34,8 @@ class NewUtilisateur
     ])]
     public UtilisateurJSB $utilisateur;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->utilisateur = new UtilisateurJSB();
     }
 
@@ -33,7 +43,23 @@ class NewUtilisateur
     public function enregistrerUtilisateurJSB()
     {
         $this->utilisateur->setCreatedAt(new DateTimeImmutable("now"));
-        dd($this->utilisateur);
+        // dd($this->utilisateur);
+        $this->saved = true;
+        $this->validated = true;
+    }
+
+    #[LiveAction]
+    public function saveAndRedirect(): RedirectResponse
+    {
+        $this->utilisateur->setCreatedAt(new DateTimeImmutable("now"));
+        return $this->redirectToRoute('app_page_index');
+    }
+
+
+    #[LiveAction]
+    public function afficherParams(#[LiveArg('id')] int $id, #[LiveArg('nom')] string $nom)
+    {
+        dd("Résultat de la saisie :", $id, $nom);
         $this->saved = true;
     }
 }
